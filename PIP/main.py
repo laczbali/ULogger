7fd46e17972174fb1bc6e0f024e78abf112b3178
Balazs Laczko <laczbali@gmail.com>
2022-08-20 23:31:30 +0200

    trying to get loglevel working in python

--- PIP/main.py
+++ PIP/main.py
@@ -1,5 +1,6 @@
-from ULoggerNET import ULog, TextLogger
+from enum import Enum
+from ULoggerNET import ULog, TextLogger, LogLevel
 
 logger = ULog()
-logger.AddLogger(TextLogger(["c:\\temp\\outj.json"]))
-logger.Log("is this working?")
+logger.AddLogger(TextLogger("c:\\temp\\out.txt"))
+logger.Log("is this working?", LogLevel.Error)
