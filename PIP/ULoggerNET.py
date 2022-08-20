dd2884ba3643487827b4392109cdcba3035c3ed3
Balazs Laczko <laczbali@gmail.com>
2022-08-20 23:13:55 +0200

    added some hint comments

--- PIP/ULoggerNET.py
+++ PIP/ULoggerNET.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import os
 import sys
 import clr
@@ -8,17 +9,46 @@ clr.AddReference("ULogger")
 # below is the definition for the ULogger classes
 # they will get loaded at the bottom with the import
 
-class ULog:
-    def Log(msg):
-        pass
-
-    def AddLogger():
-        pass
-
 class TextLogger:
+    """
+    Set up a new TextLogger
+     
+    Select where should it create a log.
+    It can be an existing or a new file.
+    The format can be .txt or .json.
+    Multiple files can be provided
+    """
     def __init__(self, filepaths) -> None:
         pass
     pass
+
+class LogLevel(Enum):
+    Info = 0,
+    Warn = 1,
+    Error = 2,
+
+class ULog:
+    """
+    Create and use loggers
+    """
+
+    def AddLogger(newLoggers : TextLogger):
+        """
+        Define a new Logger to be used when Log is called
+
+        If multiple are provided, each will be called
+
+        Can provide one or more logger
+        """
+        pass
+
+    def Log(msg : str, level : LogLevel):
+        """
+        Creates a log with each added logger
+        """
+        pass
+
+
 
 
 from ULogger import ULog # type: ignore
--- PIP/main.py
+++ PIP/main.py
@@ -3,5 +3,3 @@ from ULoggerNET import ULog, TextLogger
 logger = ULog()
 logger.AddLogger(TextLogger(["c:\\temp\\outj.json"]))
 logger.Log("is this working?")
-
-# use https://github.com/dotnet/ILMerge ?
